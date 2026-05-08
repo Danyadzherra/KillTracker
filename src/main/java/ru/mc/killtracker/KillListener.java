81ff8f4f57b582704bdb7c3653b4ea01b1888ddb
Danyadzherra <danyadzhera@gmail.com>
2026-05-08 07:06:15 +0300

    Create KillListener.java

---
+++ src/main/java/ru/mc/killtracker/KillListener.java
@@ -0,0 +1,23 @@
+package ru.mc.killtracker;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+public class KillListener implements Listener {
+    private final KillTracker plugin;
+
+    public KillListener(KillTracker plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player victim = event.getEntity();
+        Player killer = victim.getKiller();
+        if (killer != null) {
+            plugin.addKill(killer.getName());
+        }
+    }
+}
